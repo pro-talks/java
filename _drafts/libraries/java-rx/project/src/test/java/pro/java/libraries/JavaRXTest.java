ff6e22908bb9ddf4c9adaae73d444af4a9fc8bb0
Serzh Nosov <ns.projava@gmail.com>
2020-02-03 18:28:53 +0300

    JavaRX

--- _drafts/libraries/java-rx/project/src/test/java/pro/java/libraries/JavaRXTest.java
+++ _drafts/libraries/java-rx/project/src/test/java/pro/java/libraries/JavaRXTest.java
@@ -1,4 +1,92 @@
 package pro.java.libraries;
 
+
+import io.reactivex.rxjava3.core.Observable;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertEquals;
+
 public class JavaRXTest {
+
+    @Test
+    public void javaRXTest1(){
+
+        AtomicReference<String> result = new AtomicReference<>();
+        Observable<String> observable = Observable.just("Hello");
+        observable.subscribe(result::set);
+
+        assertEquals("Hello", result.get());
+
+
+        /*Observable<String> o = Observable.fromArray("a", "b", "c");
+
+        def list = [5, 6, 7, 8]
+        Observable<Integer> o = Observable.from(list);
+
+        Observable<String> o = Observable.just("one object");*/
+    }
+
+    @Test
+    public void javaRXTest2() {
+        AtomicReference<String> result = new AtomicReference<>("");
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
+        Observable<String> observable = Observable.fromArray(letters);
+        observable.subscribe(
+                i -> result.set(result.get() + i),  //OnNext
+                Throwable::printStackTrace, //OnError
+                () -> result.set(result.get() + "_Completed") //OnCompleted
+        );
+        assertEquals("abcdefg_Completed", result.get());
+    }
+
+
+    @Test
+    public void javaRXTest3() {
+        AtomicReference<String> result = new AtomicReference<>("");
+        String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
+        Observable.fromArray(letters)
+                .map(String::toUpperCase)
+                .subscribe(letter -> result.set(result.get() + letter));
+        assertEquals("ABCDEFG", result.get());
+    }
+
+    @Test
+    public void javaRXTest4() {
+        AtomicReference<String> result = new AtomicReference<>("");
+        String[] letters = {"a", "b", "c"};
+        Observable.fromArray(letters)
+                .scan(new StringBuilder(), StringBuilder::append)
+                .subscribe(total -> result.set(result.get() + total.toString()));
+        assertEquals("aababc", result.get());
+    }
+
+    @Test
+    public void javaRXTest5() {
+        AtomicReference<String> EVEN = new AtomicReference<>("");
+        AtomicReference<String> ODD = new AtomicReference<>("");
+        Observable.fromArray(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .groupBy(i -> 0 == (i % 2) ? "EVEN" : "ODD")
+                .subscribe(group ->
+                        group.subscribe(number -> {
+                            if (number != null) {
+                                if (group.getKey().equals("EVEN")) {
+                                    EVEN.set(EVEN.get() + number);
+                                } else {
+                                    ODD.set(ODD.get() + number);
+                                }
+                            }
+                        })
+                );
+        assertEquals("0246810", EVEN.get());
+        assertEquals("13579", ODD.get());
+    }
+
+
+    Observable.from(numbers)
+            .filter(i -> (i % 2 == 1))
+            .subscribe(i -> result += i);
+
+    assertTrue(result.equals("13579"));
 }
--- _drafts/libraries/java-rx/project/src/test/java/pro/java/libraries/domain/HelloWorld.java
+++ _drafts/libraries/java-rx/project/src/test/java/pro/java/libraries/domain/HelloWorld.java
@@ -2,14 +2,24 @@ package pro.java.libraries.domain;
 
 import io.reactivex.rxjava3.core.Flowable;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Serzh Nosov (Serzh.Nosov@lanit-tercom.com) created on 27.01.20.
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Flowable.just("Hello world")
-                .subscribe(System.out::println);
+        /*Flowable.just("Hello world")
+                .subscribe(System.out::println);*/
+        Integer[] objects = (Integer[]) Arrays.asList(1, 3, 5, 7, 99).toArray();
+        Flowable.fromArray(objects)
+                .subscribe(s -> System.out.println("Hello " + s + "!"));
     }
+
+    /*public static void hello(String... args) {
+
+    }*/
 
 }
